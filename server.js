b413cd5583dc29fec18964c4b741ca9ac6bde7eb
Shakhan <khan.vinn@gmail.com>
2021-07-13 19:55:53 +0500

    implement in machine  memory

--- server.js
+++ server.js
@@ -23,6 +23,31 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+let linkTotal = []
+
+app.get("/api/shorturl/:id",(req,res)=>{
+  res.redirect(linkTotal[req.params.id])
+})
+
+
+app.post("/api/shorturl",(req,res)=>{
+  const urlSent = req.body["url"]
+  var urlDns= (urlSent.split('//')[1]?.split("/")[0]) || "error-link"
+  dns.lookup(urlDns,(error)=>{
+    if(!error){
+      linkTotal.indexOf(urlSent)===-1 && linkTotal.push(urlSent)
+      res.json({
+        original_url:urlSent, 
+        short_url: linkTotal.indexOf(urlSent)
+      })
+    } else{
+      res.json({
+        message:"Invalid URL"
+      })
+    }
+  })
+})
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
