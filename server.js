68e005dd5def186dfa67e7b64699130eecbb372b
Shakhan <khan.vinn@gmail.com>
2021-07-15 12:08:42 +0500

    connected to mongodb and write any changes to db with mongoose

--- server.js
+++ server.js
@@ -1,59 +1,107 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require("body-parser")
-const dns = require("dns")
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const port = process.env.PORT || 3000;
-require('dotenv').config();
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGO_ATLASS_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
+const urlSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  link: {
+    type: String,
+    required: true,
+  },
+});
 
-app.use(bodyParser.urlencoded({extended:false}))
+const UrlModel = mongoose.model("Url", urlSchema);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-let linkTotal = []
-
-app.get("/api/shorturl/:id",(req,res)=>{
-  res.redirect(linkTotal[req.params.id])
-})
-
-
-app.post("/api/shorturl",(req,res)=>{
-  const urlSent = req.body["url"]
-  var dd = urlSent.split('//')[1]
-  var urlDns
-  if(dd){
-    urlDns= (dd.split("/")[0])
-  } else{
-    urlDns= "error-link"
-  }
-  dns.lookup(urlDns,(error)=>{
-    if(!error){
-      linkTotal.indexOf(urlSent)===-1 && linkTotal.push(urlSent)
-      res.json({
-        original_url:urlSent, 
-        short_url: linkTotal.indexOf(urlSent)
-      })
-    } else{
-      res.json({
-        error:"invalid url"
-      })
+app.get("/api/shorturl/:id", (req, res) => {
+  UrlModel.findOne({ _id: req.params.id }, (error, data) => {
+    if (error) {
+      return res.json({ error: "Dont find link with this :_id" });
     }
-  })
-})
+    res.redirect(data.link);
+  });
+});
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  const urlSent = req.body["url"];
+  var dd = urlSent.split("//")[1];
+  var urlDns;
+  if (dd) {
+    urlDns = dd.split("/")[0];
+  } else {
+    urlDns = "error-link";
+  }
+
+  dns.lookup(urlDns, (error) => {
+    if (!error) {
+      UrlModel.find({ link: urlSent }, (error, data) => {
+        if (error) {
+          return res.json({
+            error: "something was wrong",
+          });
+        }
+
+        if (data.length >= 1) {
+          return res.json({
+            original_url: urlSent,
+            short_url: data[0]._id,
+          });
+        } else if (data.length <= 0) {
+          let saveUrl = new UrlModel({
+            link: urlSent,
+            _id: new mongoose.Types.ObjectId(),
+          });
+
+          saveUrl.save((error) => {
+            console.log("saving link......")
+            if (error) {
+              return res.json({
+                error: "something was wrong",
+              });
+            }
+          });
+          res.json({
+            original_url: urlSent,
+            short_url: saveUrl._id,
+          });
+        }
+      });
+    } else {
+      res.json({
+        error: "invalid url",
+      });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
